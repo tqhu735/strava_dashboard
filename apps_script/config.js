7fb8c45245ba71d8cc709cbb103c715572020ae1
Trisan Huynh <trisanhuynh@gmail.com>
2026-01-14 07:01:53 +1300

    fix: Update activity multipliers, add multiplier for Workout

--- apps_script/config.js
+++ apps_script/config.js
@@ -10,17 +10,18 @@ const CLUB_ID = props.getProperty('CLUB_ID');
 const DISCORD_WEBHOOK_URL = props.getProperty('DISCORD_WEBHOOK_URL');
 
 const TEAMS = {
-  'Srikar': ['Srikar', 'Wilco', 'Trisan', 'Jared', 'Raymond'],
+  'Srikar': ['Srikar', 'Wilco', 'Trisan', 'Jared', 'Raymond', "Minnie", "Grace"],
   'Ravi': ['Ravi', 'Andy', 'Scott', 'Ben', 'Tommy']
 };
 
 const MULTIPLIERS = {
   'Hike': 0.5,
-  'Ride': 0.35,
+  'Ride': 0.3,
   'Rowing': 0.75,
   'Run': 1.0,
-  'Swim': 3.5,
-  'Walk': 0.1
+  'Swim': 4.0,
+  'Walk': 0.2,
+  'Workout': 0.5
 };
 
 const DISCORD_COLOR = 15548997; // Strava-esque Orange/Red
